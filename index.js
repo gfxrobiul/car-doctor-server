e14d7a399a20469b296ac7f5f2b83b17c3fefc16
gfxrobiul <mrhrobiul008877@gmail.com>
2024-05-04 13:01:42 +0600

    bookings update using patch method

--- index.js
+++ index.js
@@ -67,13 +67,28 @@ async function run() {
         res.send(result);
     });
 
+    // upload bookings put
+    app.patch('/bookings/:id',async(req, res) =>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updatedBooking = req.body;
+      console.log(updatedBooking);
+      const updateDoc ={
+        $set:{
+          status: updatedBooking.status
+        }
+      };
+      const result = await checkOutCollection.updateOne(filter,updateDoc);
+      res.send(result);
+
+    })
+
     //delete 
     app.delete('/bookings/:id', async(req,res) =>{
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
       const result = await checkOutCollection.deleteOne(query);
       res.send(result);
-
     })
 
 
